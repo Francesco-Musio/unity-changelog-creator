af505b37f8961b0eb95c75cb7c00dfac78c43ca3
Francesco Musio <f.musio@whatwapp.com>
2022-08-09 23:08:31 +0200

    feat: removed changelog and path options form cli commands
    Configs will be generated from code with all the custom options already included

--- cliOptionsProvider.js
+++ cliOptionsProvider.js
@@ -6,17 +6,6 @@ module.exports = yargs
     .option("t", { alias: "target", describe: "ready for release or WIP", type: "string", demandOption: false, default: "develop", choices: ["develop", "pre-release", "release"]})
     .option("s", { alias: "skip", describe: "skip a specific step", type: "array", demandOption: false, choices: ["bump", "changelog", "commit"]})
     .option("r", { alias: "releaseAs", description: "override semantic versioning", type: "string", demandOption: false, choices: ["major", "minor", "patch"]})
-    .option("p", { alias: "psPath", describe: "configure the Project Settings path", type: "string", demandOption: false})
-    .option("c", { alias: "changelogPath", describe: "configure the Changelog path", type: "string", demandOption: false})
-    .check((argv) => {
-    if (argv.c && !fs.lstatSync(argv.c).isDirectory()) {
-        throw Error('specified changelog path is invalid: ' + argv.c)
-    } else if (argv.p && !fs.lstatSync(argv.p).isFile()) {
-        throw Error('specified project settings path is invalid: ' + argv.p)
-    } else {
-        return true
-    }
-    })
     .alias('version', 'v')
     .alias('help', 'h')
     .wrap(120)
