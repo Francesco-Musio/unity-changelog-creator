24dbd905485034c0a796d89b3bddd5db619af08b
Francesco Musio <f.musio@whatwapp.com>
2022-08-12 11:07:14 +0200

    fix: removed debug logs from bin/configure

--- bin/configure.js
+++ bin/configure.js
@@ -5,8 +5,6 @@ if (process.version.match(/v(\d+)\./)[1] < 6) {
 		'changelog-creator: Node v6 or greater is required. `changelog-creator` did not run.'
 	)
 } else {
-    console.log("lol");
-	const config = require('../index.js');
-    console.log("lol");
+    const config = require('../index.js');
     config.CreateConfigs();
 }
